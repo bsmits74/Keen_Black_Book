47c46d44b8339a5f75ef84d920c5bf8044f76619
bsmits74 <superbassmits@gmail.com>
2023-01-07 17:01:16 +0100

    Add explanation for 2 rows of tiles

---
+++ src/code/wrap_page_buffer.c
@@ -0,0 +1,24 @@
+#define	SCREENSPACE		(SCREENWIDTH*240)
+#define FREEEGAMEM		(0x10000l-3l*SCREENSPACE)
+
+screenmove = deltay*16*SCREENWIDTH + deltax*TILEWIDTH;
+for (i=0;i<3;i++)
+{
+  screenstart[i]+= screenmove;
+  if (compatability && screenstart[i] > (0x10000l-SCREENSPACE) )
+  {
+    //
+    // move the screen to the opposite end of the buffer
+    //
+    screencopy = screenmove>0 ? FREEEGAMEM : -FREEEGAMEM;
+    oldscreen = screenstart[i] - screenmove;
+    newscreen = oldscreen + screencopy;
+    screenstart[i] = newscreen + screenmove;
+    // Copy the screen to new location
+    VW_ScreenToScreen (oldscreen,newscreen,
+       PORTTILESWIDE*2,PORTTILESHIGH*16);
+
+    if (i==screenpage)
+      VW_SetScreen(newscreen+oldpanadjust,oldpanx & xpanmask);
+  }
+}
