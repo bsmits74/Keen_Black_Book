26a031da912d4c68718e088ff636a02df48a974b
bsmits74 <superbassmits@gmail.com>
2023-02-19 16:07:47 +0100

    created asset chapter

---
+++ src/code/assets_header.c
@@ -0,0 +1,21 @@
+//////////////////////////////////////
+//
+// Graphics .H file for .KDR
+// IGRAB-ed on Fri Sep 10 11:18:07 1993
+//
+//////////////////////////////////////
+
+typedef enum {
+    #define CTL_STARTUPPIC        4
+    #define CTL_HELPUPPIC         5
+    #define CTL_DISKUPPIC         6
+    #define CTL_CONTROLSUPPIC     7
+    #define CTL_SOUNDUPPIC        8
+    #define CTL_MUSICUPPIC        9
+    #define CTL_STARTDNPIC       10
+    #define CTL_HELPDNPIC        11
+    #define CTL_DISKDNPIC        12
+    #define CTL_CONTROLSDNPIC    13
+    ...
+    #define BOOBUSWALKR4SPR     366
+    #define BOOBUSJUMPSPR       367
