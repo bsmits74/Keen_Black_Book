bd19355e1810431495b787fc187837be763e7005
bsmits74 <superbassmits@gmail.com>
2023-06-04 14:53:26 +0200

    Start with input section

--- src/code/angle.c
+++ src/code/angle.c
@@ -2,31 +2,22 @@ absx = abs(ob->xspeed);
 absy = ob->yspeed;
 if (absx>absy)
 {
-    if (absx>absy*2)        // 22 degrees
-    {
-        angle = 0;
-        speed = absx*286;   // x*sqrt(5)/2 *256
-    }
-    else
-    [...]
+	if (absx>absy*2)        // 22 degrees
+	{
+		angle = 0;
+		speed = absx*286;     // x*sqrt(5)/2 *256
+	}
+	else
+	[...]             // Check for 45, 67 and 90 degrees      
 }
 
 if (ob->xspeed > 0)       
-    angle = 7-angle;        // mirror angle
+	angle = 7-angle;        // mirror angle
 
-speed >>= 1;                // speed / 2 after bounce
+speed >>= 1;              // speed / 2 after bounce
 newangle = bounceangle[ob->hitnorth][angle];
 switch (newangle)
 {
-case 0:
-	ob->xspeed = speed / 286;
-	ob->yspeed = -ob->xspeed / 2;
-	break;
-case 1:
-	ob->xspeed = speed / 362;
-	ob->yspeed = -ob->xspeed;
-	break;
-
 [...]
 
 case 5:
---
+++ src/code/keyboard.c
@@ -0,0 +1,25 @@
+#define	KeyInt		9	// The keyboard ISR number
+
+static void INL_StartKbd(void) {
+
+	IN_ClearKeysDown();
+
+	OldKeyVect = getvect(KeyInt);
+	setvect(KeyInt,INL_KeyService);
+
+	INL_KeyHook = 0;	// Clear key hook
+}
+
+static void interrupt INL_KeyService(void) {
+  byte	k;
+  k = inportb(0x60);	// Get the scan code
+
+  // Tell the XT keyboard controller to clear the key
+  outportb(0x61,(temp = inportb(0x61)) | 0x80);
+  outportb(0x61,temp);
+
+  [...] // Process scan code.
+  Keyboard[k] = XXX;
+
+  outportb(0x20,0x20); // ACK interrupt to interrupt system.
+}
