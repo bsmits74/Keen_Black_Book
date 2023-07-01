750d1a0b3be3e933733bc7a31408529ef83f07aa
bsmits74 <superbassmits@gmail.com>
2023-07-01 16:10:03 +0200

    Completed CGA section

---
+++ src/code/cga_calc_origin.c
@@ -0,0 +1,12 @@
+void RFL_CalcOriginStuff (long x, long y)
+{
+    [...]	
+
+    originxglobal = x;
+    originyglobal = y;
+	
+    panx = (originxglobal>>G_P_SHIFT) & 15;
+    pansx = panx & 12;  //pansx is 0, 4, 8 or 12 pixels
+    pany = pansy = (originyglobal>>G_P_SHIFT) & 15;
+    panadjust = pansx/4 + ylookup[pansy];
+}
---
+++ src/code/cga_screen_refresh.c
@@ -0,0 +1,38 @@
+void VW_CGAFullUpdate (void)
+{
+    displayofs = bufferofs+panadjust;
+
+    asm	mov	ax,0xb800
+    asm	mov	es,ax
+
+    asm	mov	si,[displayofs]
+    asm	xor	di,di
+
+    asm	mov	bx,100          // pairs of scan lines to copy
+    asm	mov	dx,[linewidth]
+    asm	sub	dx,80
+
+    asm	mov	ds,[screenseg]  // buffer segment in memory
+    asm	test	si,1
+    asm	jz	evenblock
+
+    [...]
+
+    evenblock:
+    asm	mov	ax,40           // words accross screen
+    copytwolines:
+    asm	mov	cx,ax
+    asm	rep	movsw           // copy row to VRAM bank 0
+    asm	add	si,dx
+    asm	add	di,0x2000-80    // go to the interlaced bank 1
+    asm	mov	cx,ax
+    asm	rep	movsw           // copy row to VRAM bank 1
+    asm	add	si,dx
+    asm	sub	di,0x2000   // go to the non interlaced bank 0
+
+    asm	dec	bx
+    asm	jnz	copytwolines
+
+[...]
+
+}
