8028921a034467b297425fca36524fbd90c09d9c
bsmits74 <superbassmits@gmail.com>
2024-03-24 17:58:51 +0100

    work update

---
+++ src/code/write_ega.c
@@ -0,0 +1,20 @@
+# define SC_INDEX   0x3C4
+# define SC_DATA    0x3C5
+# define SC_MAPMASK 0x02
+
+char far *EGA = (unsigned char far*)0xA0000000L;
+
+void selectPlane (int plane) {
+  outp ( SC_INDEX , SC_MAPMASK );
+  outp ( SC_DATA , 1 << plane );
+}
+
+void WriteScreen(void){
+  int i, bank_id;
+
+  for (bank_id = 0 ; bank_id < 4 ; bank_id++) {
+    selectPlane(bank_id);
+    for (i = 0; i < 40 * 200; i++) {
+      EGA[i] = 0x00;
+  }
+}
