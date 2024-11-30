67b6d6385e6f3c47c2a0d8c64e524ebac201458b
bsmits74 <superbassmits@gmail.com>
2024-11-30 20:39:39 +0100

    Completed Software chapter

--- src/code/VW_MaskBlock.asm
+++ src/code/VW_MaskBlock.asm
@@ -1,10 +1,3 @@
-maskroutines	dw	mask0,mask0,mask1E,mask1E,mask2E,mask2O,mask3E,mask3O
-				dw	mask4E,mask4O,mask5E,mask5O,mask6E,mask6O
-				dw	mask7E,mask7O,mask8E,mask8O,mask9E,mask9O
-				dw	mask10E,mask10O
-
-routinetouse	dw	?
-
 PROC	VW_MaskBlock	segm:WORD, ofs:WORD, dest:WORD, wide:WORD, height:WORD, planesize:WORD
 
 [...]
---
+++ src/code/VW_maskroutines.asm
@@ -0,0 +1,6 @@
+maskroutines	
+  dw	mask0,mask0,mask1E,mask1E,mask2E,mask2O,mask3E,mask3O
+  dw	mask4E,mask4O,mask5E,mask5O,mask6E,mask6O
+  dw	mask7E,mask7O,mask8E,mask8O,mask9E,mask9O
+  dw	mask10E,mask10O
+
